a394c75e1d9bf186e1aeadd7997d2d580809a5d2
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-01-29 16:33:23 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,23 @@
 class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+  end
+  def show
+    @student = Student.find(params[:id])
+  end
+  def new 
+  end
+  def create
+    
+  end
+  def update
+    
+  end
   
-  
+    private
+    
+      def 
+        
+      end
+
 end
